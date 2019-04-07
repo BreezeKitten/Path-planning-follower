d079b67ef7b920ce795cfd9d27e8ecdd3e07d846
BreezeKitten <48701080+BreezeKitten@users.noreply.github.com>
2019-04-07 22:32:41 +0800

    Update PPF.py

--- path_planning_follower/src/PPF.py
+++ path_planning_follower/src/PPF.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
+
+
+# edited @ 20190405 
 import numpy as np
 import matplotlib.pyplot as plt
 import math as m
 import rospy
 from geometry_msgs.msg import Twist
-from geometry_msgs.msg import Ponit
+from geometry_msgs.msg import Point
+from nav_msgs.msg import Odometry
 
-from turtlesim.msg import Pose
 
-V_avg = 0.2
+V_avg = 0.25
 V_max = 0.25
-W_max = 0.5
+W_max = 1
 S = 0.0
 A = 0
 deltaT = 0.02
@@ -24,7 +27,9 @@ t = 0
 Xnow = 0
 Ynow = 0
 yaw = 0
-theta_final = 0
+theta_final = m.pi*3/2
+Vcom = 0
+Wcom = 0
 
 cmd_vel_topic = "cmd_vel"
 com_pub = rospy.Publisher(cmd_vel_topic,Twist,queue_size=10)
@@ -50,7 +55,7 @@ def path_cal(P1,P2,P3,goal):
     else:
         theta_f = m.atan2((P3[1]-P2[1]),(P3[0]-P2[0]))
 
-    k = 10
+    k = 0.5    #TODO: change k = sqrt((X_i - X_f) ** 2 + (Y_i - Y_f) ** 2)
     ax = k * m.cos(theta_f) - 3 * X_f
     
     ay = k * m.sin(theta_f) - 3 * Y_f
@@ -73,7 +78,7 @@ def path_cal(P1,P2,P3,goal):
     
     A = round(k/(V_avg*deltaT))
     if goal == True:
-        for j in range(0,A+2):
+        for j in range(0,int(A)+2):
             X_d.append(np.polyval(Xs,j/A))
             Y_d.append(np.polyval(Ys,j/A))
     else:
@@ -86,9 +91,9 @@ def path_cal(P1,P2,P3,goal):
         X_d_dot.append((X_d[i+1] - X_d[i])/deltaT)
         Y_d_dot.append((Y_d[i+1] - Y_d[i])/deltaT)
         theta_d.append(m.atan2(Y_d_dot[i],X_d_dot[i]))
-        if i > 0:
-            if(theta_d[i] > theta_d[i-1] + m.pi):
-                theta_d[i] = theta_d[i] - 2 * m.pi
+ #       if i > 0:
+ #           if(theta_d[i] > theta_d[i-1] + m.pi):
+  #              theta_d[i] = theta_d[i] - 2 * m.pi
         V_d.append(m.sqrt((X_d_dot[i])**2 + (Y_d_dot[i])**2))
 
     for i in range(0,int(A)):
@@ -108,11 +113,14 @@ def Vel_command(X,Y,theta,X_d,Y_d,theta_d,V_d,W_d):
     e2 = -(X_d - X) * m.sin(theta) + (Y_d - Y) * m.cos(theta)
     e3 = theta_d - theta
     u1 = -k1 * e1
+    if(abs(e3) > m.pi):
+	e3 = e3 + 2 * m.pi
     u2 = -k2 * e2 - k3 * e3
 
     V_com = V_d * m.cos(e3) - u1
     W_com = W_d - u2
     
+
     return V_com,W_com
 
 def command_pub(Vcom,Wcom):
@@ -128,7 +136,7 @@ def command_pub(Vcom,Wcom):
     vel_msg.angular.z = Wcom
     
     com_pub.publish(vel_msg)
-    rospy.loginfo("command pub")	
+ #   rospy.loginfo("command pub")	
     t = t + 1
  
     return
@@ -137,17 +145,24 @@ def posecallback(data):
     global Xnow
     global Ynow
     global yaw 
-    Xnow = data.x
-    Ynow = data.y
-    yaw = data.theta
-    print(Xnow,Ynow,yaw)
+    global Vcom
+    global Wcom
+    Xnow = data.pose.pose.position.x
+    Ynow = data.pose.pose.position.y
+    yaw = data.pose.pose.orientation.z
+
     if t < len(Vd)-1:
         Vcom,Wcom = Vel_command(Xnow,Ynow,yaw,Xd[t],Yd[t],thetad[t],Vd[t],Wd[t])
     else:
         Vcom = 0
         Wcom = 0
+    
+    if(abs(Vcom) > V_max):
+	Vcom = np.sign(Vcom) * V_max
+    if(abs(Wcom) > W_max):
+	Wcom = np.sign(Wcom) * W_max
+    print(Vcom,Wcom)
 
-    command_pub(Vcom,Wcom)
 
     return
 
@@ -159,20 +174,24 @@ def subgoalCB(data):
     global Vd
     global Wd
     global t
-    subgoal_x = data.x
-    subgoal_y = data.y
-    Xd,Yd,thetad,Vd,Wd = path_cal([Xnow,Ynow],[subgoal_x,subgoal_y],[7,4],True)
+    subgoal_x = data.linear.x
+    subgoal_y = data.linear.y
+    direct_x = data.linear.z
+    direct_y = data.angular.x
+    Xd,Yd,thetad,Vd,Wd = path_cal([Xnow,Ynow],[subgoal_x,subgoal_y],[direct_x,direct_y],False)
     t = 0
-    rospy.loginfo("subgoal change")
+
     return
 
 
 
 if __name__ == '__main__':
-#    Xd,Yd,thetad,Vd,Wd = path_cal([5,5],[9,4],[7,4],False)
-    rospy.init_node('PPF',anonymous=True)
-    rate = rospy.Rate(100)
-    rospy.Subscriber("/robot_pose",Pose,posecallback)
-    rospy.Subscriber("/subgoal_position",Ponit,subgoalCB)
-    rospy.spin()
 
+    rospy.init_node('PPF',anonymous=True)
+    rate = rospy.Rate(50)    # 50 = 1 / deltaT
+    sub = rospy.Subscriber("/robot_pose",Odometry,posecallback)
+    sub2 = rospy.Subscriber("/subgoal_position",Twist,subgoalCB)
+    while not rospy.is_shutdown():
+	command_pub(Vcom,Wcom)
+	rate.sleep()	 
+    
